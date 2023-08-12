42f820a0f24e18675cad70a5cdddcff9d42c4d22
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-08-12 18:18:16 -0400

    Landing page template

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def landing
+  end
+
+  def app
+  end
+end
---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Pages", type: :request do
+  describe "GET /home" do
+    it "returns http success" do
+      get "/pages/home"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /landing" do
+    it "returns http success" do
+      get "/pages/landing"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /app" do
+    it "returns http success" do
+      get "/pages/app"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
